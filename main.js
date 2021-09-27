1457691474d38ca99d6547223d65c33497b2dfa2
yohanesteban <emunozmontero04@gmail.com>
2021-09-27 17:17:08 -0500

    105-108

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ window.onload = () => {
         console.log(todoText);
 }
 }
+
+function iterar(arg1) {
+    for(let i = 0; i < arg1.length; i++) {
+        console.log(arg1[i]);
+    }  
+}
+const numeros = [1,2,'hola',4,5]
+const nombres = ['pedro','juan','felipe','chanchito feliz','chanchito triste']
+iterar(nombres)
+iterar(numeros)
+
+
+function suma(a, b) {
+    return a + b;
+}
+
+const resultadosuma1 = suma(1,2)
+const resultadosuma2 = suma(5,6)
+const resultadosuma3 = suma(resultadosuma1,resultadosuma2)
+console.log('resultado',resultadosuma3);
