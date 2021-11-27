2f52f8639a6daa6f7b96424306d957eaf54f0746
yohanesteban <emunozmontero04@gmail.com>
2021-11-26 19:08:50 -0500

    117-122

--- main.js
+++ main.js
@@ -25,3 +25,45 @@ window.onload = () => {
       })
     }
   }
+
+//   new promise ((resolve.reject) => {
+//     setTimeOut( => resolve(2), 1000)
+// }
+// .then(x => console.log(x))  
+// .catch(e => console.error(encode))  
+
+// Promise.resolve(2)
+// Promise { <state>: "fulfilled", <value>: 2 }
+
+// Promise.resolve(2)          .then(valor => console.log(valor))
+// 2 debugger eval code:1:45
+// Promise { <state>: "fulfilled", <value>: undefined }
+// ​
+// <state>: "fulfilled"
+// ​
+// <value>: undefined
+
+// Promise.resolve(2)          .then(valor => valor + 1) .then(valor =>console.log(valor) )
+// 3 debugger eval code:1:70
+// Promise { <state>: "fulfilled", <value>: undefined }
+
+// Promise.resolve(2)          .then(valor => valor + 1000000000) .then(valor =>console.log(valor) )
+// 1000000002 debugger eval code:1:79
+// Promise { <state>: "fulfilled", <value>: undefined }
+
+// Promise.resolve(10)
+// Promise { <state>: "fulfilled", <value>: 10 }
+
+// Promise.resolve(2)  .then(valor =>console.log(valor) )
+// 2 debugger eval code:1:42
+// Promise { <state>: "fulfilled", <value>: undefined }
+
+// Promise.resolve(2)          .then(valor => valor + 1) .then(valor =>console.log(valor) )
+// 3 debugger eval code:1:70
+// Promise { <state>: "fulfilled", <value>: undefined }
+// ​
+// <state>: "fulfilled"
+// ​
+// <value>: undefined
+
+// .catch(e => console.error (e))
